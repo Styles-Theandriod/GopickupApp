649345d5840f946c681f2a0aaee4c47f9aede704
Styles-Theandriod <emmanuelajah188@gmail.com>
2025-08-27 15:30:38 +0100

    Gopickup Main app

---
+++ app/info2.tsx
@@ -0,0 +1,83 @@
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+export default function Info2() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        padding: 20,
+        backgroundColor: "#F8F9F8",
+      }}
+    >
+      <View
+        style={{
+          marginTop: 100,
+          alignItems: "center",
+          paddingHorizontal: 20,
+          borderRadius: 10,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 46,
+            fontStyle: "normal",
+            textAlign: "center",
+            fontWeight: "700",
+            marginBottom: 20,
+            color: "#424441",
+          }}
+        >
+          Experience Proffessional Delivery Services
+        </Text>
+      </View>
+      <View
+        style={{ marginTop: 20, alignItems: "center", paddingHorizontal: 20 }}
+      >
+        <Image
+          source={require("../assets/images/Staff.png")}
+          style={{ width: 315, height: 170 }}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 150,
+          gap: 100,
+          width: "100%",
+          paddingHorizontal: 20,
+          bottom: 50,
+          position: "absolute",
+          justifyContent: "space-between",
+          alignItems: "center",
+          height: 50,
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            borderColor: "#00000026",
+            width: 91,
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 20,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{color:'#00000026'}}>Skip</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            borderColor: "#439D25",
+            width: 91,
+            borderWidth: 1,
+            padding: 10,
+            borderRadius: 20,
+            alignItems: "center",
+          }}
+        >
+          <Text style={{color:'#439D25', fontWeight:'700', fontSize:15.83}}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
